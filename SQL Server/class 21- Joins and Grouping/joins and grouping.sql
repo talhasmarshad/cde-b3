d038211e88acb5fcbc6f95cc03ed18518cc81c17
aiwithqasim <qasimhasssan1020@gmail.com>
2025-11-09 17:01:44 +0500

    class21

---
+++ SQL Server/class 21- Joins and Grouping/joins and grouping.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{B92461AE-9FB3-4274-83F3-F6A2ED4A3F7E}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {030FEBF1-EDF5-4E5C-90B5-73364CE3E2B0}
+	EndGlobalSection
+EndGlobal
